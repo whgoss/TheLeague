6d855ebdd0a20f9f59235a817d9a97a2fabd9021
Will Goss <will.goss@sparcedge.com>
2013-08-26 11:24:18 -0400

    Initial Commit

---
+++ TheLeague/TheLeague/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  TheLeague
+//
+//  Created by Will Goss on 8/26/13.
+//  Copyright (c) 2013 Will Goss. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ TheLeague/TheLeague/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  TheLeague
+//
+//  Created by Will Goss on 8/26/13.
+//  Copyright (c) 2013 Will Goss. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
